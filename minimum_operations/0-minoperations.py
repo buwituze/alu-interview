8e1ede59c92d7385288f93f891cf217d5c08034d
Benitha Uwituze <144014295+buwituze@users.noreply.github.com>
2024-05-19 19:35:55 +0300

    task0

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,18 +2,16 @@
 """
 Module for calculating the fewest number of operations needed to result in exactly n H characters in the file.
 """
-
 def minOperations(n):
-    if n == 1:
+    """ Minimum Operations"""
+    if n <= 1:
         return 0
-    
-    dp = [0] * (n + 1)
-
-   # use  maximum possible operations
-    for i in range(2, n + 1):
-        dp[i] = i 
-        for j in range(1, i):
-            if i % j == 0: # check if j is a divisor of i
-                dp[i] = min(dp[i], dp[j] + i // j)
-    
-    return dp[n]
+    i = 2
+    res = 0
+    while i <= n:
+        if n % i == 0:
+            res += i
+            n = n / i
+        else:
+            i += 1
+    return res
