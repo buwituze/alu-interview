1084a81137f57d0404b6db2e131c2b3d9989e401
Benitha Uwituze <144014295+buwituze@users.noreply.github.com>
2024-05-19 19:38:50 +0300

    validating

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-Module for calculating the fewest number of 
+Module for calculating the fewest number of
 operations needed to result in exactly n H characters in the file.
 """
+
+
 def minOperations(n):
     """ Minimum Operations"""
     if n <= 1:
